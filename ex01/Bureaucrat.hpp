084993693a60709e133d266fc0e3462ff9071d3f
love <tlove.tky@gmail.com>
2023-08-15 15:57:07 +0900

    ex01: first commit

---
+++ ex01/Bureaucrat.hpp
@@ -0,0 +1,47 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+#define RED		"\e[0;31m" // Red
+#define GREEN	"\e[0;32m" // Green
+#define YELLOW	"\e[0;33m" // Yellow
+#define BLUE	"\e[0;34m" // Blue
+#define RESET 	"\e[0m"    // Reset
+
+class Bureaucrat
+{
+	private:
+		const std::string	_name;
+		int					_grade;
+	public:
+		Bureaucrat();
+		Bureaucrat(std::string name, int grade);
+		~Bureaucrat();
+
+		Bureaucrat(const Bureaucrat& rhs);
+		Bureaucrat& operator=(const Bureaucrat& rhs);
+
+		const std::string&	getName(void) const;
+		int					getGrade(void) const;
+
+		void	increaseGrade(void);
+		void	decreaseGrade(void);
+		void	checkOverGrade(const int grade) const;
+
+		class GradeTooHighException
+		{
+			public:
+				const char* what() const throw();
+		};
+		class GradeTooLowException
+		{
+			public:
+				const char* what() const throw();
+		};
+
+};
+
+std::ostream&	operator<<(std::ostream& os, const Bureaucrat& rhs);
+
+#endif
---
+++ ex01/main.cpp
@@ -0,0 +1,91 @@
+#include "Bureaucrat.hpp"
+
+int main( void )
+{
+	Bureaucrat bureaucrat1;
+
+	std::cout << BLUE <<
+	"------------------------------------------" << std::endl <<
+	"               Initial Test "	 			<< std::endl <<
+	"------------------------------------------" << std::endl <<
+	RESET << std::endl;
+	for (int i = 149; i < 153; i++)
+	{
+		try
+		{
+			std::cout << "i = " << i << std::endl;
+			bureaucrat1 = Bureaucrat("bureaucrat" + std::to_string(i), i);
+			std::cout << GREEN << bureaucrat1 << RESET << std::endl;
+		}
+		catch(Bureaucrat::GradeTooLowException& e)
+		{
+			std::cout << RED << e.what() << "  " << RESET << std::endl;
+			continue;
+		}
+		catch(Bureaucrat::GradeTooHighException& e)
+		{
+			std::cout << RED << e.what() << RESET << std::endl;
+			continue;
+		}
+	}
+
+	std::cout << BLUE <<
+	"------------------------------------------" << std::endl <<
+	"             IncreaseGrade Test "	 			<< std::endl <<
+	"------------------------------------------" << std::endl <<
+	RESET << std::endl;
+
+	int i = 3;
+	Bureaucrat bureaucrat2("bureaucrat", i);
+	while (i > -1)
+	{
+		try
+		{
+			std::cout << "i = " << i << " -> " << i - 1 <<std::endl;
+			bureaucrat2.increaseGrade();
+			std::cout << GREEN << bureaucrat2 << RESET << std::endl;
+		}
+		catch(Bureaucrat::GradeTooLowException& e)
+		{
+			std::cout << RED << e.what() << "  " << RESET << std::endl;
+		}
+		catch(Bureaucrat::GradeTooHighException& e)
+		{
+			std::cout << RED << e.what() << RESET << std::endl;
+		}
+		i--;
+	}
+
+	std::cout << BLUE <<
+	"------------------------------------------" << std::endl <<
+	"             DecreaseGrade Test "	 			<< std::endl <<
+	"------------------------------------------" << std::endl <<
+	RESET << std::endl;
+
+	i = 148;
+	Bureaucrat bureaucrat3("bureaucrat", i);
+	while (i < 152)
+	{
+		try
+		{
+			std::cout << "i = " << i << " -> " << i + 1 <<std::endl;
+			bureaucrat3.decreaseGrade();
+			std::cout << GREEN << bureaucrat3 << RESET << std::endl;
+		}
+		catch(Bureaucrat::GradeTooLowException& e)
+		{
+			std::cout << RED << e.what() << "  " << RESET << std::endl;
+		}
+		catch(Bureaucrat::GradeTooHighException& e)
+		{
+			std::cout << RED << e.what() << RESET << std::endl;
+		}
+		i++;
+	}
+	return	0;
+}
+
+__attribute((destructor))
+static void destructor() {
+	system("leaks -q main");
+}
