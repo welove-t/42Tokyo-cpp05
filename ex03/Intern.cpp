4ce76e0c688d9cd12927a94f58d920a902f80936
love <tlove.tky@gmail.com>
2023-08-19 14:13:35 +0900

    ex03:InternクラスにformNameメンバ変数を追加

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -2,13 +2,17 @@
 
 /* Orthodox Canonical Form */
 Intern::Intern()
+: formName{"shrubbery creation", "robotomy request", "presidential pardon"}
 {
 	std::cout << "(Intern) Default Constructor called" << std::endl;
+
 }
 
 Intern::Intern(const Intern& rhs)
 {
 	std::cout << "(Intern) Copy Constructor called" << std::endl;
+	 for (int i = 0; i < formSize; ++i)
+		const_cast<std::string&>(formName[i]) = rhs.formName[i];
 }
 
 Intern::~Intern()
@@ -21,7 +25,13 @@ Intern& Intern::operator=(const Intern& rhs)
 	std::cout << "(Intern) Copy assignment operator called" << std::endl;
 	if (this == &rhs)
 		return *this;
+	for (int i = 0; i < formSize; ++i)
+		const_cast<std::string&>(formName[i]) = rhs.formName[i];
 	return *this;
 }
 
 /* Function */
+AForm*	Intern::makeForm(std::string form, std::string target)
+{
+
+}
--- ex03/Intern.hpp
+++ ex03/Intern.hpp
@@ -6,12 +6,16 @@
 class Intern
 {
 	private:
+		static const int	formSize = 3;
+		std::string			formName[formSize];
 	public:
 		Intern();
 		~Intern();
 
 		Intern(const Intern& rhs);
 		Intern& operator=(const Intern& rhs);
+
+		AForm*	makeForm(std::string form, std::string target);
 };
 
 
